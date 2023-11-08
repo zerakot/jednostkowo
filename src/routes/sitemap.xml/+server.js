8c271881670a431a5eb0340fc67111e59678bb24
Wiktor <iktorn2006@wp.pl>
2023-11-08 19:12:46 +0100

    Added sitemap

---
+++ src/routes/sitemap.xml/+server.js
@@ -0,0 +1,44 @@
+import blog from '$lib/assets/blog.json';
+import { calculators } from '$lib/assets/calculators.js';
+import { website } from '$lib/assets/seo.js';
+
+export async function GET({ fetch, setHeaders }) {
+	setHeaders({
+		'Content-Type': 'application/xml'
+	});
+
+	const blogMap = blog.posts
+		.map(
+			(post) => `
+            <url>
+            <loc>${website.baseUrl}/blog/${post.slug}</loc>
+            </url>`
+		)
+		.join('');
+
+	const calculatorsMap = calculators
+		.map(
+			(calculator) => `
+        <url>
+            <loc>${website.baseUrl}/${calculator?.type}-${calculator?.id}</loc>
+        </url>`
+		)
+		.join('');
+
+	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>${website.baseUrl}</loc>
+        </url>
+        <url>
+            <loc>${website.baseUrl}/kontakt</loc>
+        </url>
+        <url>
+            <loc>${website.baseUrl}/kontakt</loc>
+        </url>
+
+        ${blogMap}
+        ${calculatorsMap}
+        </urlset>`;
+	return new Response(sitemap);
+}
