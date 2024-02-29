7e43201dbfb64816f1d74d349324838d1b100f47
Wiktor <iktorn2006@wp.pl>
2024-02-29 22:28:38 +0100

    Bug fixed

--- src/lib/components/calculators/Percentage/Operation.svelte
+++ src/lib/components/calculators/Percentage/Operation.svelte
@@ -11,7 +11,8 @@
 	$: {
 		if (a === null || b === null) result = 0;
 		else {
-			result = formatOutputNumber(round(operation.formula(a, b), 2));
+			result = formatOutputNumber(round(operation.formula(parseFloat(a), parseFloat(b)), 2));
+			console.log(operation.formula(a, b));
 		}
 	}
 </script>
